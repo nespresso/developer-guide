294163e446134ea1f2ea78fdf4c5e16dc3a00a15
Joshua Muheim <josh@muheimwebdesign.ch>
2015-07-04 12:34:45 +0200

    Release ready!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,3 @@ josh = User.create! do |user|
        end
 
 josh.add_role :admin
-
-marc = User.create! do |user|
-         user.name         = 'marc'
-         user.email        = 'marc@example.com'
-         user.password     = 'marcmarc'
-         user.confirmed_at = Time.now
-       end
